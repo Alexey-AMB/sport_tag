25c6a788fefae112fb57ab9c3f6369c8fb8438f6
Alexey-AMB <alexey@ambintech.ru>
2019-03-26 14:20:10 +0300

    Initial commit

---
+++ FlashROM/Application/subdir_vars.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/bim_util.c \
+../Application/board_key.c \
+../Application/hal_assert.c \
+../Application/simple_peripheral_oad_offchip.c \
+../Application/simple_peripheral_oad_offchip_menu.c \
+../Application/two_btn_menu.c \
+../Application/util.c 
+
+C_DEPS += \
+./Application/bim_util.d \
+./Application/board_key.d \
+./Application/hal_assert.d \
+./Application/simple_peripheral_oad_offchip.d \
+./Application/simple_peripheral_oad_offchip_menu.d \
+./Application/two_btn_menu.d \
+./Application/util.d 
+
+OBJS += \
+./Application/bim_util.obj \
+./Application/board_key.obj \
+./Application/hal_assert.obj \
+./Application/simple_peripheral_oad_offchip.obj \
+./Application/simple_peripheral_oad_offchip_menu.obj \
+./Application/two_btn_menu.obj \
+./Application/util.obj 
+
+OBJS__QUOTED += \
+"Application\bim_util.obj" \
+"Application\board_key.obj" \
+"Application\hal_assert.obj" \
+"Application\simple_peripheral_oad_offchip.obj" \
+"Application\simple_peripheral_oad_offchip_menu.obj" \
+"Application\two_btn_menu.obj" \
+"Application\util.obj" 
+
+C_DEPS__QUOTED += \
+"Application\bim_util.d" \
+"Application\board_key.d" \
+"Application\hal_assert.d" \
+"Application\simple_peripheral_oad_offchip.d" \
+"Application\simple_peripheral_oad_offchip_menu.d" \
+"Application\two_btn_menu.d" \
+"Application\util.d" 
+
+C_SRCS__QUOTED += \
+"../Application/bim_util.c" \
+"../Application/board_key.c" \
+"../Application/hal_assert.c" \
+"../Application/simple_peripheral_oad_offchip.c" \
+"../Application/simple_peripheral_oad_offchip_menu.c" \
+"../Application/two_btn_menu.c" \
+"../Application/util.c" 
+
+
---
+++ FlashROM/Startup/subdir_vars.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+D:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/blestack/target/board.c \
+../Startup/find_stack_entry.c \
+../Startup/main.c 
+
+C_DEPS += \
+./Startup/board.d \
+./Startup/find_stack_entry.d \
+./Startup/main.d 
+
+OBJS += \
+./Startup/board.obj \
+./Startup/find_stack_entry.obj \
+./Startup/main.obj 
+
+OBJS__QUOTED += \
+"Startup\board.obj" \
+"Startup\find_stack_entry.obj" \
+"Startup\main.obj" 
+
+C_DEPS__QUOTED += \
+"Startup\board.d" \
+"Startup\find_stack_entry.d" \
+"Startup\main.d" 
+
+C_SRCS__QUOTED += \
+"D:/ti/simplelink_cc2640r2_sdk_2_40_00_32/source/ti/blestack/target/board.c" \
+"../Startup/find_stack_entry.c" \
+"../Startup/main.c" 
+
+
